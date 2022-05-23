58eacb1a3e3e24d535c1e5e39d2b8a9698b8e38c
Davi Santos Silveira <Davitpontas@gmail.com>
2022-05-22 21:32:01 -0300

    Update Ex002.py

--- Exercicios01/Ex002.py
+++ Exercicios01/Ex002.py
@@ -1,14 +1,15 @@
+#jogo de par ou impar em python
 from random import randint
 rand = randint(0, 59)
-jogo = str(input('Escolha par ou Impar: '))
+jogo = str(input('Escolha Par ou Impar: ')).upper
 logica = rand%2
 if logica == 0:
     log = 'Par'
 else:
     log = 'Impar'
-if logica == 0 and jogo == 'par':
+if logica == 0 and jogo == 'PAR':
     print(f'Voce Ganhou! pois {rand} é {log}')
-elif logica == 1 and jogo == 'impar':
+elif logica == 1 and jogo == 'IMPAR':
     print(f'Voce Ganhou! pois {rand} é {log}')
 else:
     print(f'Voce Perdeu! pois {rand} é {log}')
