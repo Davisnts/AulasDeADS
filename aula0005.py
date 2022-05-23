bc643d6f5b4c60630d5a31f0bb50ed7cdf7453cb
Davi Santos Silveira <Davitpontas@gmail.com>
2022-05-22 21:28:56 -0300

    Update aula0005.py

--- aula0005.py
+++ aula0005.py
@@ -1,12 +1,12 @@
-#Faça um programa que leia o nome do vendendor,e atribua  seu salario com base na tabela
-# ---> ate 1  ano =  R$ 1280,00
-# ---> de 1 ate 3 anos  = R$ 1690,00
-# ---> 3 ate 5 anos = R$ 2100,00
-# Calcule tambem a comissao sobre as vendas, respeitando a tabela
-# ---> vendas inferior ou igual a 20.000,00 comissoes de 5%
-# ---> vendas acima de 20.000,00 e menos que 30.000,00 comissão de 8%
-# ---> vendas acima de 30.000,00 - comissoes de 12%
-#Ao Final salario total do vendendor
+'''Faça um programa que leia o nome do vendendor,e atribua  seu salario com base na tabela
+---> ate 1  ano =  R$ 1280,00
+---> de 1 ate 3 anos  = R$ 1690,00
+---> 3 ate 5 anos = R$ 2100,00
+Calcule tambem a comissao sobre as vendas, respeitando a tabela
+---> vendas inferior ou igual a 20.000,00 comissoes de 5%
+---> vendas acima de 20.000,00 e menos que 30.000,00 comissão de 8%
+---> vendas acima de 30.000,00 - comissoes de 12%
+Ao Final salario total do vendendor'''
 
 func = str(input('Qual o seu nome ? '))
 anos = int(input(f'Caro {func} Quanto tempo você trabalha nessa empresa? '))
